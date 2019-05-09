d07dc4bb7acd6f5d22c40388a538c6e1b516809c
hiran@unicorn-solutions.com <hiran@unicorn-solutions.com>
2019-05-09 16:00:17 +0530

    ch

--- pymongoTest.py
+++ pymongoTest.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 import json
-
+from collections import namedtuple
+# get secrets from json file. its not there in repo
 f=open("credentials.json",'r')
 y = json.loads(f.read())
 
 client = MongoClient(y["mongoconnection"])
 db = client.sample_weatherdata
 collection = db.data
-print(collection.find_one({'elevation': 9999}))
+print(collection.find_one({'elevation': 9999})['precipitationEstimatedObservation']["estimatedWaterDepth"])
